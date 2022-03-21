6189f41199df8812deb80227079f1a54e3dc6e58
jason <luhao19900625@hotmail.com>
2022-03-20 22:26:15 -0400

    commit

--- utils/notification_util.py
+++ utils/notification_util.py
@@ -2,7 +2,6 @@ import smtplib
 from email.message import EmailMessage
 from etl_init import ALERT_CONFIG
 
-from twilio.rest import Client
 from etl_init import ALERT_CONFIG
 
 def email_alert(subject, body, to):
@@ -22,16 +21,3 @@ def email_alert(subject, body, to):
 
     server.quit()
 
-
-
-def SMS_alert(message):
-
-    client = Client(ALERT_CONFIG["SMS_account_sid"], ALERT_CONFIG["SMS_auth_token"])
-    message = client.messages.create(body=message,
-                                     from_=ALERT_CONFIG["SMS_source"],
-                                     to=ALERT_CONFIG["SMS_destination"])
-    print(message.sid)
-    sent_msg = client.messages(message.sid).fetch()
-    print(f"message sid: {message.sid}, contents: {sent_msg.body}")
-
-    return None
