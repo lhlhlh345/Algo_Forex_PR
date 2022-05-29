5e5f49e746a8ddeb0189262ae14ef076f7957ce5
jason <luhao19900625@hotmail.com>
2022-05-29 12:50:01 -0400

    commit

--- 003_incremental_load_forex_1m_candle.py
+++ 003_incremental_load_forex_1m_candle.py
@@ -144,7 +144,10 @@ print('Time Elapsed: ', end - start)
 
 
 # ==========================================Send Email Alert Notification===================================================
-msg = f"The ETL job for refreshing forex_precious_metal_1m_jason_stage table is completed on {dt.date.today().strftime('%Y/%m/%d')}. Sending at {dt.datetime.now().strftime('%H:%M:%S.%f')}, and elapsed time is {Elapsed_Time}. Update Message: {body}"
+msg = f"The ETL job for refreshing forex_precious_metal_1m_jason_stage table is completed on {dt.date.today().strftime('%Y/%m/%d')}. \n" \
+      f"Sending at {dt.datetime.now().strftime('%H:%M:%S.%f')}," \
+      f" and elapsed time is {Elapsed_Time}. \n" \
+      f"Update Message: {body} "
 
 if __name__ == '__main__':
     email_alert("Algo Trading ETL process update", msg, "emailalertjasonlu900625@gmail.com;algotraders.investors@gmail.com")
